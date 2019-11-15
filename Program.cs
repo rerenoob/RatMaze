1a10bf46662f9e191a412acc40b4fe290dcc5f88
rerenoob <dphamsw@gmail.com>
2019-11-14 20:19:39 -0600

    Rearranged functions

--- Program.cs
+++ Program.cs
@@ -4,45 +4,7 @@ namespace RatMaze
 {
     class Program
     {
-
-        static void PrintSolution(int[,] solution){
-            for (int i = 0; i < solution.GetLength(0); i++){
-                for (int j = 0; j < solution.GetLength(1); j++){
-                     Console.Write(solution[i,j] + " ");
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine();
-        }
-
-        static bool ValidMove(int x, int y, int[,] solution)
-        {
-            return (x >= 0 && x < maze.GetLength(0) &&
-                    y >= 0 && y < maze.GetLength(1) &&
-                    maze[x,y] == 1 && solution[x,y] == 0);
-        }
-
-        static bool RatMazeSolver(int x, int y, int[,] solution){
-            if (x == destX && y == destY) // base case: maze solved
-            {
-                return true;
-            }
-
-            int nextX, nextY;
-
-            for (int i = 0; i < PossibleMoveX.Length; i++){
-                nextX = x + PossibleMoveX[i];
-                nextY = y + PossibleMoveY[i];
-                if (ValidMove(nextX, nextY, solution)){                  
-                    solution[nextX, nextY] = 1;
-                    if (RatMazeSolver(nextX, nextY, solution))
-                        return true;
-                    solution[nextX, nextY] = 0;
-                }
-            }
-            return false;
-        }
-
+        // Change the mazeSize and actual maze to create new maze
         const int mazeSize = 6;
         static int[,] maze = new int [mazeSize, mazeSize] {
             {1, 0, 1, 1, 1, 0},
@@ -82,6 +44,44 @@ namespace RatMaze
             }else{
                 Console.WriteLine("Solution doesn't exist.");
             }
+        }
+        
+        static void PrintSolution(int[,] solution){
+            for (int i = 0; i < solution.GetLength(0); i++){
+                for (int j = 0; j < solution.GetLength(1); j++){
+                     Console.Write(solution[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        static bool ValidMove(int x, int y, int[,] solution)
+        {
+            return (x >= 0 && x < maze.GetLength(0) &&
+                    y >= 0 && y < maze.GetLength(1) &&
+                    maze[x,y] == 1 && solution[x,y] == 0);
+        }
+
+        static bool RatMazeSolver(int x, int y, int[,] solution){
+            if (x == destX && y == destY) // base case: maze solved
+            {
+                return true;
+            }
+
+            int nextX, nextY;
+
+            for (int i = 0; i < PossibleMoveX.Length; i++){
+                nextX = x + PossibleMoveX[i];
+                nextY = y + PossibleMoveY[i];
+                if (ValidMove(nextX, nextY, solution)){                  
+                    solution[nextX, nextY] = 1;
+                    if (RatMazeSolver(nextX, nextY, solution))
+                        return true;
+                    solution[nextX, nextY] = 0;
+                }
+            }
+            return false;
         }
     }
 }
